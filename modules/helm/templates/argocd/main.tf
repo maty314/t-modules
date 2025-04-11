b42bf195122431a814eebf12ed6f7cdb1770ff18
Matias Pi <39473398+maty314@users.noreply.github.com>
2025-04-11 15:33:16 -0300

    Update main.tf

--- modules/helm/templates/argocd/main.tf
+++ modules/helm/templates/argocd/main.tf
@@ -1,10 +1,9 @@
-#module "helm" {
-#  source = "git::git@gitlab.poincenot.net:infra/provisioner/modules.git//helm"
-#  cluster_name  = var.cluster_name
-#  access_key  = var.access_key
-#  secret_key = var.secret_key
-#  helm_releases = var.helm_releases
-#  region        = var.region
-#}
+module "helm" {
+  source = "git::git@gitlab.poincenot.net:infra/provisioner/modules.git//helm"
 
-
+  cluster_name  = var.cluster_name
+  access_key  = var.access_key
+  secret_key = var.secret_key
+  helm_releases = var.helm_releases
+  region        = var.region
+}
