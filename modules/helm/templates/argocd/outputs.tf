5c1819d57d7dee164c1a0c1d9f5b07c8395a8728
Matias Pi <39473398+maty314@users.noreply.github.com>
2025-04-11 15:33:35 -0300

    Update outputs.tf

--- modules/helm/templates/argocd/outputs.tf
+++ modules/helm/templates/argocd/outputs.tf
@@ -1,5 +1,5 @@
-#output "outputs" {
-#  value = module.helm.outputs
-#  description = "Component output"
-#  sensitive = true
-#}
+output "outputs" {
+  value = module.helm.outputs
+  description = "Component output"
+  sensitive = true
+}
